96dcd2834a3cbfd3107f821f547abee2e2257727
dongsuk <scienceyds@naver.com>
2024-06-07 19:42:18 +0900

    server init

---
+++ server/byob/src/main/java/com/soma/byob/ByobApplication.java
@@ -0,0 +1,13 @@
+package com.soma.byob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByobApplication.class, args);
+	}
+
+}

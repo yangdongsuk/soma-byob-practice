f2bae1aa9bce3e26ffb01caae49b59511e252665
dongsuk <scienceyds@naver.com>
2024-06-12 15:06:48 +0900

    feat : log 추가

--- front/byob/src/pages/api/check-server.js
+++ front/byob/src/pages/api/check-server.js
@@ -4,6 +4,8 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json({ data: data.data });
   } catch (error) {
+    console.log("env", process.env.SPRING_PRIVATE_DNS_URL);
+    console.log("error", error);
     res.status(500).json({ data: "아쉽게도 연결 안되었넹 ㅠ." });
   }
 }
